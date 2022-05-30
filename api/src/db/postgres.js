89f87d0574d17cfbc27ad63ca4ce8659af6858f6
yorubadeveloper <bukunmiadewale3@gmail.com>
2022-05-30 22:22:05 +0100

    Dockerized the app

--- api/src/db/postgres.js
+++ api/src/db/postgres.js
@@ -1,7 +1,15 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(
-  'postgres://postgres:test@localhost:5432/api-gateway'
-);
+const sequelize = new Sequelize(process.env.DB_SCHEMA || 'api-gateway',
+    process.env.DB_USER || 'postgres',
+    process.env.DB_PASSWORD || 'postgres',
+    {
+        host: process.env.DB_HOST || 'localhost',
+        port: process.env.DB_PORT || 5432,
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: process.env.DB_SSL == "true"
+        }
+    });
 
 export default sequelize;
