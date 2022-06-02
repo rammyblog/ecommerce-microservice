b50ffbf189c715903106dcd89a171ec11d6ffda8
Samson Adejoro <s.adejoro@CONCEPTGROUP-NG.COM>
2022-06-02 12:02:54 +0100

    feedback from review

--- product-service/src/proxy.js
+++
@@ -1,9 +0,0 @@
-import { createProxyMiddleware } from 'http-proxy-middleware';
-
-const setupProxies = (app, routes) => {
-  routes.forEach((r) => {
-    app.use(r.url, createProxyMiddleware(r.proxy));
-  });
-};
-
-export default setupProxies;
