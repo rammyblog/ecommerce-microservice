e49aa96a8cfb1c23f8fe79032a1f83289470a98b
Babatunde Onasanya <onasanyatunde67@gmail.com>
2022-06-03 22:10:17 +0100

    Add migrations and seeders

--- product-service/src/models/Product.js
+++ product-service/src/models/Product.js
@@ -1,43 +1,23 @@
 //PRODUCT Schema
 const Product = (sequelize, DataTypes) => {
-  let Product = sequelize.define("Product", {
+  let Product = sequelize.define('Product', {
     name: DataTypes.STRING,
     description: DataTypes.TEXT,
-    // userId:DataTypes.INTEGER,
-    categoryId:DataTypes.INTEGER,
-    sku:DataTypes.INTEGER,
-    costPrice:DataTypes.FLOAT,
-    sellingPrice:DataTypes.FLOAT,
-    expirationDate:DataTypes.DATE,
-    availableQuantity:DataTypes.INTEGER
-    
-  })
+    // categoryId: DataTypes.INTEGER,
+    sku: DataTypes.INTEGER,
+    costPrice: DataTypes.FLOAT,
+    sellingPrice: DataTypes.FLOAT,
+    expirationDate: DataTypes.DATE,
+    availableQuantity: DataTypes.INTEGER
+  });
+  Product.associate = function (models) {
+    Product.belongsTo(models.Category, {
+      onDelete: 'CASCADE',
+      foreignKey: 'categoryId'
+    });
+  };
 
-  // Product.associate = function(models) {
-  //   Product.belongsToMany(models.Cart,{
-  //     through: "cart_products",
-  //     onDelete: 'CASCADE',
+  return Product;
+};
 
-  //     as: "carts",
-  //     // foreignKey: "cartId",
-  //   })
-  // };
-
-  // Product.associate = function(models) {
-  //   Product.belongsTo(models.User, {
-  //     onDelete: "CASCADE",
-  //     foreignKey: "userId",
-  //   })
-  // }
-
-  Product.associate = function(models) {
-      Product.belongsTo(models.Category, {
-        onDelete: "CASCADE",
-        foreignKey: "categoryId",
-      })
-    }
-
-  return Product
-}
-
-export default Product
+export default Product;
--- product-service/src/seeders/20220603144018-add-products.js
+++ product-service/src/seeders/20220603144018-add-products.js
@@ -1,48 +1,58 @@
 module.exports = {
-  up: async (queryInterface) => queryInterface.bulkInsert('Products', [
-    {
-      name: 'Strawberry',
-      description:'Some stuff',
-      sellingPrice:200.00,
-      costPrice:200.00,
-      availableQuantity:10,
-      sku:2001,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
-      name: 'Coconut',
-      description:'Some stuff',
-      sellingPrice:200.00,
-      costPrice:200.00,
-      availableQuantity:10,
-      sku:2001,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
-      name: 'Mango',
-      description:'Some stuff',
-      sellingPrice:200.00,
-      costPrice:200.00,
-      availableQuantity:10,
-      sku:2001,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-    {
-      name: 'Fruit',
-      description:'Some stuff',
-      sellingPrice:200.00,
-      costPrice:200.00,
-      availableQuantity:10,
-      sku:2001,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    },
-  
-  ], {}),
+  up: async (queryInterface) =>
+    queryInterface.bulkInsert(
+      'Products',
+      [
+        {
+          name: 'Strawberry',
+          description: 'Some stuff',
+          sellingPrice: 200.0,
+          costPrice: 200.0,
+          availableQuantity: 10,
+          sku: 2001,
+          categoryId: 3,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Coconut',
+          description: 'Some stuff',
+          sellingPrice: 200.0,
+          costPrice: 200.0,
+          availableQuantity: 10,
+          categoryId: 4,
+          sku: 2001,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Mango',
+          description: 'Some stuff',
+          sellingPrice: 200.0,
+          costPrice: 200.0,
+          availableQuantity: 10,
+          categoryId: 5,
+          sku: 2001,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'Fruit',
+          description: 'Some stuff',
+          sellingPrice: 200.0,
+          costPrice: 200.0,
+          categoryId: 4,
+          availableQuantity: 10,
+          sku: 2001,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+      {}
+    ),
   down: async (queryInterface) => {
-    await queryInterface.bulkDelete('Products', {[Op.or]: [{name: 'USD'}, {name: 'EUR'}]});
+    await queryInterface.bulkDelete('Products', {
+      [Op.or]: [{ name: 'USD' }, { name: 'EUR' }]
+    });
   }
 };
