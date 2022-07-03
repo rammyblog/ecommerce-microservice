e2486aace9e3c6bb9cb6f2f0ed5b3f617791a7c5
Mayowa <mayobami49@gmail.com>
2022-07-03 23:43:21 +0100

    resolve merge conflict

---
+++ frontend/components/SignIn/SignIn.jsx
@@ -0,0 +1,91 @@
+import { Formik, Form } from "formik";
+import Link from "next/link";
+import CustomInput from "../customInput/CustomInput";
+import Joi from 'joi';
+import {
+  Box,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  VStack,
+  Button,
+  Image,
+  Heading,
+  Text,
+} from "@chakra-ui/react";
+
+
+const SignIn = () => {
+  return (
+    <Box h="100vh" bg="white" color="gray.500">
+      <Flex justify="space-around" h="93.5vh" align="center">
+        <Box display={["none", "none", "flex"]}>
+          <Image src="login-img.svg" w={500} h={500} alt="login" />
+        </Box>
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          onSubmit={(values) => {
+            alert(JSON.stringify(values, null, 2));
+          }}>
+          {({ handleSubmit, errors, touched }) => (
+            <VStack spacing={5} align={["center", "center", "flex-start"]}>
+              <Heading color="black">Welcome Back</Heading>
+              <Form onSubmit={handleSubmit}>
+                <FormControl isInvalid={!!errors.email && touched.email}>
+                  <CustomInput
+                    label="Email Address"
+                    name="email"
+                    type="text"
+                    placeholder="Enter Email address"
+                    validate={(value) => {
+                        let error;
+                        if (value.length < 6) {
+                          error = "Enter a valid email address";
+                        }
+                        return error;
+                      }}
+
+                  />
+                  <FormErrorMessage>{errors.email}</FormErrorMessage>
+                </FormControl>
+                <FormControl  isInvalid={!!errors.password && touched.password}>
+                <CustomInput
+                  label="Enter Password"
+                  name="password"
+                  type="password"
+                  placeholder="Enter Password"
+                  validate={(value) => {
+                    let error;
+                    if (value.length < 6) {
+                      error =
+                        "Password must contain at least 6 characters";
+                    }
+
+                    return error;
+                  }}
+                />
+                <FormErrorMessage>{errors.password}</FormErrorMessage>
+                </FormControl>
+                
+              </Form>
+              <Button type="submit" colorScheme="purple" w="full">
+                Login
+              </Button>
+              <Box  w="full" display='flex' justifyContent='center'>
+                Don't have an account?
+                <Text  color="purple" ml={2}>
+                <Link href="/signup">
+                  Signup
+                </Link>
+                </Text>
+  
+              </Box>
+            </VStack>
+          )}
+        </Formik>
+      </Flex>
+    </Box>
+  );
+};
+
+export default SignIn;
---
+++ frontend/pages/login.js
@@ -0,0 +1,11 @@
+import SignIn from "../components/SignIn/SignIn";
+
+const Login = () => {
+    return (
+        <>
+        <SignIn />
+        </>
+    )
+}
+
+export default Login;
